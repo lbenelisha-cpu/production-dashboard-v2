a5a31ae8541a2daeeb5de927fafe22116b112857
lbenelisha-cpu <lbenelisha@gmail.com>
2026-09-01 17:48:50 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'iml-production-dashboard-v4.8.46';
+const CACHE_NAME = 'iml-production-dashboard-v4.8.47';
 const APP_SHELL = [
   './',
   './index.html',
-  './manifest.webmanifest?v=4.8.46',
+  './manifest.webmanifest?v=4.8.47',
   './icons/iml-icon-192-v4.png',
   './icons/iml-icon-512-v4.png',
   './icons/iml-apple-touch-icon-v4.png',
